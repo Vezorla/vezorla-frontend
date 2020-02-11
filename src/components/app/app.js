c3c160681a781e070057540f5910e60f50ab1185
J. Mark Findlay <jmarkfindlay@gmail.com>
2020-02-11 08:48:08 -0700

    Added box for UX

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Box from '@material-ui/core/Box';
 
 import img1 from '../../assets/images/img-1.JPG'
 import img2 from '../../assets/images/img-2.JPG'
@@ -49,25 +50,25 @@ function App() {
     <div className="App">
 
       <Header cart={lineItems}/>
+        <Box overflow="scroll" style={{paddingBottom: "10vh"}}>
+          <Switch>
+              {/* <Route path={["/","/index"]} exact strict component={Home}/> */}
+              {/* <Route path={["/cart","/cart/:userid"]} exact strict component={Cart}/> */}
+              <Route path="/shop" exact strict component={Shop}/>
+              {/* <Route path="/findus" exact strict component={FindUs}/> */}
+              {/* <Route path="/contact" exact strict component={Contact}/> */}
+              {/* <Route path="/about" exact strict component={About}/> */}
+              {/* <Route path="/login" exact strict component={Login}/> */}
 
-      <Switch>
-          {/* <Route path={["/","/index"]} exact strict component={Home}/> */}
-          {/* <Route path={["/cart","/cart/:userid"]} exact strict component={Cart}/> */}
-          <Route path="/shop" exact strict component={Shop}/>
-          {/* <Route path="/findus" exact strict component={FindUs}/> */}
-          {/* <Route path="/contact" exact strict component={Contact}/> */}
-          {/* <Route path="/about" exact strict component={About}/> */}
-          {/* <Route path="/login" exact strict component={Login}/> */}
+              <Route path="/product/:productid" exact strict 
+                render={({match}) => <Product prodId={match.params.productid} addCartHandle={increaseCart}/>}
+              />
 
-          <Route path="/product/:productid" exact strict 
-            render={({match}) => <Product prodId={match.params.productid} addCartHandle={increaseCart}/>}
-          />
-
-          <Route path="/404" component={NotFound}/>
-          <Redirect to="/404"/>
-      </Switch>
-      
-      {/* <Footer /> */}
+              <Route path="/404" component={NotFound}/>
+              <Redirect to="/404"/>
+          </Switch>
+        </Box>
+      <Footer />
 
     </div>
   );
