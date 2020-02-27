b538acc64eb763fce9674da01576d44e476cbf9c
Lam Minh <738623@ACDM.DS.SAIT.CA>
2020-02-27 10:50:02 -0700

    Testing

--- src/components/checkoutPage/Payment.jsx
+++ src/components/checkoutPage/Payment.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
+
+// Minh wqas here
 
 export default function Payment() {
-    return (
-        <div>
-            payment
-        </div>
-    )
+	return <div>payment</div>;
 }
