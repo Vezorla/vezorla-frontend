94b92dad78f0e2b8d54d2fa8b125914a4f5f8d06
Minh Lam <ltm1701@gmail.com>
2020-03-02 14:39:37 -0700

    re commit since last one has error

--- src/components/common/header/customerOverlay.js
+++ src/components/common/header/customerOverlay.js
@@ -19,9 +19,7 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import ExploreIcon from "@material-ui/icons/Explore";
 import StoreIcon from "@material-ui/icons/Storefront";
-import {
-  NavLink
-} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const useStyles = makeStyles({
   paper: {
@@ -63,332 +61,128 @@ export default function TemporaryDrawer() {
       return;
     }
 
-    setState({
-      ...state,
-      [side]: open
-    });
+    setState({ ...state, [side]: open });
   };
 
-  const sideList = side => ( <
-    div className = {
-      classes.list
-    }
-    role = "presentation"
-    onClick = {
-      toggleDrawer(side, false)
-    }
-    onKeyDown = {
-      toggleDrawer(side, false)
-    } >
-    <
-    List className = {
-      classes.boxSpacing
-    } >
-    <
-    Box >
-    <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/"
-    exact >
-    <
-    ListItem button key = {
-      "Home"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    HomeIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "Home"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/customer/cart"
-    exact >
-    <
-    ListItem button key = {
-      "Cart"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    ShoppingCartIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "Cart"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/customer/shop"
-    exact >
-    <
-    ListItem button key = {
-      "Shop"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    StoreIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "Shop"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    Divider style = {
-      {
-        marginBottom: "15px",
-        marginTop: "15px",
-        backgroundColor: "#D0C50A",
-        width: "90%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        height: "3px",
-        borderRadius: "1.5px"
-      }
-    }
-    /> <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/findus"
-    exact >
-    <
-    ListItem button key = {
-      "Find us"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    ExploreIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "Find Us"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/contact"
-    exact >
-    <
-    ListItem button key = {
-      "Contact Us"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    MailOutlineIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "Contact Us"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    Divider style = {
-      {
-        marginBottom: "15px",
-        marginTop: "15px",
-        backgroundColor: "#D0C50A",
-        width: "90%",
-        marginLeft: "auto",
-        marginRight: "auto"
-      }
-    }
-    /> <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/about"
-    exact >
-    <
-    ListItem button key = {
-      "About Us"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    BookmarkIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "About Us"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    Divider style = {
-      {
-        marginBottom: "15px",
-        marginTop: "15px"
-      }
-    }
-    /> <
-    Box boxShadow = {
-      3
-    }
-    padding = "5px" >
-    <
-    NavLink to = "/login" >
-    <
-    ListItem button key = {
-      "Sign In"
-    }
-    className = {
-      classes.type
-    } >
-    <
-    ListItemIcon >
-    <
-    AccountCircleIcon className = {
-      classes.icon
-    }
-    /> <
-    /ListItemIcon> <
-    ListItemText primary = {
-      "Sign In"
-    }
-    classes = {
-      {
-        primary: classes.text
-      }
-    }
-    /> <
-    /ListItem> <
-    /NavLink> <
-    /Box> <
-    /Box> <
-    /List> <
-    /div>
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List className={classes.boxSpacing}>
+        <Box>
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/" exact>
+              <ListItem button key={"Home"} className={classes.type}>
+                <ListItemIcon>
+                  <HomeIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"Home"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/customer/cart" exact>
+              <ListItem button key={"Cart"} className={classes.type}>
+                <ListItemIcon>
+                  <ShoppingCartIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"Cart"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/customer/shop" exact>
+              <ListItem button key={"Shop"} className={classes.type}>
+                <ListItemIcon>
+                  <StoreIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"Shop"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+          <Divider
+            style={{
+              marginBottom: "15px",
+              marginTop: "15px",
+              backgroundColor: "#D0C50A",
+              width: "90%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              height: "3px",
+              borderRadius: "1.5px"
+            }}
+          />
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/findus" exact>
+              <ListItem button key={"Find us"} className={classes.type}>
+                <ListItemIcon>
+                  <ExploreIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"Find Us"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/contact" exact>
+              <ListItem button key={"Contact Us"} className={classes.type}>
+                <ListItemIcon>
+                  <MailOutlineIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"Contact Us"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+          <Divider
+            style={{
+              marginBottom: "15px",
+              marginTop: "15px",
+              backgroundColor: "#D0C50A",
+              width: "90%",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+          />
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/about" exact>
+              <ListItem button key={"About Us"} className={classes.type}>
+                <ListItemIcon>
+                  <BookmarkIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"About Us"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+          <Divider style={{ marginBottom: "15px", marginTop: "15px" }} />
+          <Box boxShadow={3} padding="5px">
+            <NavLink to="/login">
+              <ListItem button key={"Sign In"} className={classes.type}>
+                <ListItemIcon>
+                  <AccountCircleIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary={"Sign In"} classes={{primary: classes.text}} />
+              </ListItem>
+            </NavLink>
+          </Box>
+        </Box>
+      </List>
+    </div>
   );
 
-  return ( <
-    div >
-    <
-    Button onClick = {
-      toggleDrawer("left", true)
-    } >
-    <
-    MenuIcon style = {
-      {
-        color: "#0C3658"
-      }
-    }
-    /> <
-    /Button> <
-    Drawer classes = {
-      {
-        paper: classes.paper
-      }
-    }
-    open = {
-      state.left
-    }
-    onClose = {
-      toggleDrawer("left", false)
-    } >
-    {
-      sideList("left")
-    } <
-    /Drawer> <
-    /div>
+  return (
+    <div>
+      <Button onClick={toggleDrawer("left", true)}>
+        <MenuIcon style={{ color: "#0C3658" }} />
+      </Button>
+      <Drawer
+        classes={{ paper: classes.paper }}
+        open={state.left}
+        onClose={toggleDrawer("left", false)}
+      >
+        {sideList("left")}
+      </Drawer>
+    </div>
   );
 }
