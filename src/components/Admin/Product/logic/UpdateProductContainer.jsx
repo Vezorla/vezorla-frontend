aa0de237a8f001a764d903020cdc27823ab647b8
Minh Lam <ltm1701@gmail.com>
2020-03-18 11:23:55 -0600

    Still dont know how the back end handle file therefore stop at this point

--- src/components/Admin/Product/logic/UpdateProductContainer.jsx
+++ src/components/Admin/Product/logic/UpdateProductContainer.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import PopUp from '../../../common/';
+import PopUp from '../../../common/PopUp/PopUp';
+import Product from '../view/Product';
+import LoadingHOC from '../../../common/HOC/LoadingHOC';
 
 export default class UpdateProductContainer extends Component {
 	constructor() {
@@ -22,6 +24,67 @@ export default class UpdateProductContainer extends Component {
 		};
 	}
 
+	setIndex = (value) => this.setState({ index: value });
+
+	setCost = (e) => {
+		this.setState({ info: { ...this.state.info, cost: e.target.value } });
+	};
+
+	setPrice = (e) => {
+		this.setState({ info: { ...this.state.info, price: e.target.value } });
+	};
+
+	setQuantity = (e) => {
+		this.setState({ info: { ...this.state.info, quantity: e.target.value } });
+	};
+
+	setWarehouse = (e) => {
+		this.setState({ info: { ...this.state.info, warehouse: e.target.value } });
+	};
+
+	setDescription = (e) => {
+		this.setState({ info: { ...this.state.info, description: e.target.value } });
+	};
+
+     // ---------------Re-deceide how to send img------------------
+	addImg = async (e) => {
+		const file = e.target.files[0];
+		try {
+			const response = await fetch('url', {
+				method: 'POST',
+				header: {
+					// If doesn't work change into img/xyz
+					'Content-Type': 'img/*'
+				},
+				credentials: 'include',
+				body: file
+			});
+
+			if (response.status === 200) {
+				this.fetchData();
+			}
+		} catch (err) {}
+	};
+
+	delImg = async (e) => {
+		try {
+			const response = await fetch('url', {
+				method: 'DELETE',
+				header: {
+					'Content-Type': 'application-json'
+				},
+				credentials: 'include',
+				body: JSON.stringify(this.state.index)
+			});
+		} catch (err) {
+			this.setState({ error: true, message: 'image is not deleted' });
+		}
+	};
+
+	setError = () => {
+		this.setState({ error: !this.state.error });
+	};
+
 	fetchData = async () => {
 		this.setState({ stage: 'loading' });
 		try {
@@ -36,13 +99,49 @@ export default class UpdateProductContainer extends Component {
 		}
 	};
 
-	onCancel = () => {
+	goBack = () => {
 		this.props.history.push('/admin/inventory');
 	};
 
-	onSave;
+	onSave = async () => {
+		try {
+			const response = await fetch('url', {
+				method: 'POST',
+				header: {
+					'Content-Type': 'application/json'
+				},
+				credentials: 'include',
+				body: JSON.stringify({ ...this.state.info })
+			});
+			if (response.status === 200) {
+				this.setState({ success: true, message: 'Product Updated' });
+			} else {
+				this.setState({ error: true, message: 'Something Wrong' });
+			}
+		} catch (err) {
+			this.setState({ error: true, message: 'Something Wrong' });
+		}
+	};
 
 	render() {
-		return <div />;
+		return (
+			<div>
+				{this.state.error ? <PopUp label="Error" message={this.state.message} onClick={this.setError} /> : ''}
+				{this.state.success ? <PopUp label="Success" message={this.state.message} onClick={this.goBack} /> : ''}
+				{LoadingHOC(Product)({
+					...this.state,
+					addImg: this.addImg,
+					delImg: this.delImg,
+					setCost: this.setCost,
+					setPrice: this.setPrice,
+					setQuantity: this.setQuantity,
+					setWarehouse: this.setWarehouse,
+					setDescription: this.setDescription,
+					onSave: this.onSave,
+					onCancel: this.goBack,
+					setIndex: this.setIndex
+				})}
+			</div>
+		);
 	}
 }
