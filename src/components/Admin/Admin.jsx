9fc2f774d6daf14b18bc917533378b922838e4c0
Carlos J. Garcia Carmona <carlos.garciacarmona@edu.sait.ca>
2020-03-30 20:50:54 -0600

    Basic Purchase components and routing

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import ClientPage from './Clients/logic/ClientPage';
 import ClientInfoContainer from './ClientInfo/logic/ClientInfoContainer';
 import SettingContainer from './Setting/logic/SettingContainer';
 import CreateProductContainer from './Product/logic/CreateProductContainer';
+import PurchaseContainer from "./Purchase/Purchase-container";
 
 export default function Admin() {
-	return (
-		<Switch>
-			<Route path="/admin/settings" exact strict component={SettingContainer} />
-			<Route path="/admin/client" exact strict component={ClientPage} />
-			<Route paht="/admin/inventory/create" exact strict component={CreateProductContainer} />
-			<Route
-				path="/admin/client/:clientid"
-				exact
-				strict
-				render={({ match }) => <ClientInfoContainer clientId={match.params.productid} />}
-			/>
-			<Redirect path="/admin/403" />
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route path="/admin/settings" exact strict component={SettingContainer}/>
+      <Route path="/admin/client" exact strict component={ClientPage}/>
+      <Route path="/admin/inventory/create" exact strict component={CreateProductContainer}/>
+      <Route
+        path="/admin/client/:clientid"
+        exact
+        strict
+        render={({match}) => <ClientInfoContainer clientId={match.params.productid}/>}
+      />
+      <Route path="/admin/purchases" exact strict component={PurchaseContainer}/>
+      <Redirect to="/admin/403"/>
+    </Switch>
+  );
 }
---
+++ src/components/Admin/Purchase/Purchase-view.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Container, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
+export default function Purchase() {
+  const classes = useStyles();
+
+  return (
+    <Container disableGutters>
+
+    </Container>
+  );
+}
