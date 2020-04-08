7eb16b4ae1910c31551165fd3c99e19df4882b55
Carlos J. Garcia Carmona <carlos.garciacarmona@edu.sait.ca>
2020-04-08 17:42:02 -0600

    UI for Login

--- src/components/login/logic/LoginContainer.jsx
+++ src/components/login/logic/LoginContainer.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 import Login from '../view/Login';
-import { withRouter } from 'react-router-dom';
 
 /**
- * @file Login View Componenet 
+ * @file Login View Componenet
  * @author MinhL4m
  * @version 1.0
  */
@@ -14,84 +14,82 @@ const URL = 'http://localhost:8080/api/auth/login';
  * Login Logic class component
  */
 class LoginContainer extends Component {
-	/**
-	 * Constructor
-	 * @param {setAuth} set authentication (client, admin) 
-	 */
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: '',
-			password: '',
-			error: ''
-		};
-		this.onClick = this.onClick.bind(this);
-		this.setPassword = this.setPassword.bind(this);
-		this.setEmail = this.setEmail.bind(this);
-	}
+  /**
+   * Constructor
+   * @param {setAuth} set authentication (client, admin)
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    };
+    this.onClick = this.onClick.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+  }
 
-	//----Setter-----
-	setEmail = (emailVal) => {
-		this.setState({ email: emailVal });
-	};
+  //----Setter-----
+  setEmail = (emailVal) => {
+    this.setState({email: emailVal});
+  };
 
-	setPassword = (e) => {
-		this.setState({ password: e.target.value });
-	};
+  setPassword = (e) => {
+    this.setState({password: e.target.value});
+  };
 
-	/**
-	 * Handle on log in
-	 * Post request to server to verify the email and password
-	 */
-	onClick = async () => {
-		if (
-			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-				this.state.email
-			)
-		) {
-			try {
-				const response = await fetch(URL, {
-					method: 'POST',
-					headers: {
-						Content: 'application/json',
-						Accept: 'application/json'
-					},
-					body: JSON.stringify({ email: this.state.email, password: this.state.password }),
-					credentials: 'include'
-				});
+  /**
+   * Handle on log in
+   * Post request to server to verify the email and password
+   */
+  onClick = async () => {
+    if (
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        this.state.email
+      )
+    ) {
+      try {
+        const response = await fetch(URL, {
+          method: 'POST',
+          headers: {
+            Content: 'application/json',
+            Accept: 'application/json'
+          },
+          body: JSON.stringify({email: this.state.email, password: this.state.password}),
+          credentials: 'include'
+        });
 
-				//if 401 (Unauthorized) then error message
-				if (response.status === 401) {
-					this.setState({ error: 'Invalid username or password', password: '' });
-				} else if (response.status === 200) {
-					//if 200 (ok)
-					const data = await response.json();
-					//login as admin
-					if (data.admin === true) {
-						this.props.setAuth('admin');
-						this.props.history.push('/admin/dashboard');
-					} else {
-						//login as client
-						this.props.setAuth('client');
-						this.props.history.push('/');
-					}
-				}
-			} catch (err) {
-				this.setState({ error: 'Error has occured! Please try again later.', password: '' });
-			}
-		}
-	};
+        //if 401 (Unauthorized) then error message
+        if (response.status === 401) {
+          this.setState({error: 'Invalid username or password', password: ''});
+        } else if (response.status === 200) {
+          //if 200 (ok)
+          const data = await response.json();
+          //login as admin
+          if (data.admin === true) {
+            this.props.setAuth('admin');
+            this.props.history.push('/admin/dashboard');
+          } else {
+            //login as client
+            this.props.setAuth('client');
+            this.props.history.push('/');
+          }
+        }
+      } catch (err) {
+        this.setState({error: 'Error has occured! Please try again later.', password: ''});
+      }
+    }
+  };
 
-	/**
-	 * @returns Login component that Login Logic wrap around Login View
-	 */
-	render() {
-		return (
-			<div>
-				<Login {...this.state} setPassword={this.setPassword} setEmail={this.setEmail} onClick={this.onClick} />
-			</div>
-		);
-	}
+  /**
+   * @returns Login component that Login Logic wrap around Login View
+   */
+  render() {
+    return (
+      <Login {...this.state} setPassword={this.setPassword} setEmail={this.setEmail} onClick={this.onClick}/>
+    );
+  }
 }
 
 export default withRouter(LoginContainer);
--- src/components/login/view/Login.jsx
+++ src/components/login/view/Login.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { TextField, Button, Typography } from '@material-ui/core';
-import { NavLink } from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom';
+import {
+  Button,
+  Container,
+  FormControl,
+  makeStyles,
+  TextField,
+  Typography
+} from '@material-ui/core';
 import EmailInput from '../../common/Inputs/Email/EmailInput';
+import globalStyles from "../../../assets/styles/styles";
 
 /**
- * @file Login View Componenet 
+ * @file Login View Componenet
  * @author MinhL4m
  * @version 1.0
  */
+
+const useStyles = makeStyles(theme => ({
+  containerMain: {
+    height: "90vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  containerCentered: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  containerButtons: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  forgot: {
+    margin: "1rem 0",
+    textAlign: "center"
+  },
+  button: {
+    margin: "1rem 0"
+  }
+}));
 
 /**
  * Login Functional Component
@@ -19,66 +53,94 @@ import EmailInput from '../../common/Inputs/Email/EmailInput';
  * @param {function} onClick - function handle submit username and password
  * @returns Login View Component
  */
-export default function Login({ email, password, error = '', setEmail, setPassword, onClick }) {
-	React.useEffect(() => {
-		const btn = document.querySelector('.login-btn');
-		const pass = document.querySelector('.pass');
-		const email = document.querySelector('.email');
+export default function Login({email, password, error = '', setEmail, setPassword, onClick}) {
+  React.useEffect(() => {
+    const btn = document.querySelector('#login-btn');
+    const pass = document.querySelector('#pass-textfield');
+    const email = document.querySelector('.email-textfield');
 
-		function enter(e) {
-			if (e.keyCode === 13) {
-				e.preventDefault();
+    function enter(e) {
+      if (e.keyCode === 13) {
+        e.preventDefault();
 
-				btn.click();
-			}
-		}
+        btn.click();
+      }
+    }
 
-		pass.addEventListener('keyup', enter);
-		email.addEventListener('keyup', enter);
-		return () => {
-			pass.removeEventListener('keyup', enter);
-			email.removeEventListener('keyup', enter);
-		};
-	}, []);
+    pass.addEventListener('keyup', enter);
+    email.addEventListener('keyup', enter);
+    return () => {
+      pass.removeEventListener('keyup', enter);
+      email.removeEventListener('keyup', enter);
+    };
+  }, []);
 
-	return (
-		<div>
-			<div>
-				<Typography variant="h4" component="h4">
-					Welcome
-				</Typography>
-				<Typography variant="h4" component="h4">
-					Sign in to continue
-				</Typography>
-			</div>
-			<div>
-				<EmailInput className="email" helperText="Invalid Email" onChange={setEmail} value={email} />
-				<TextField className="pass" label="Password" type="password" value={password} onChange={setPassword} />
-			</div>
-			<div>
-				{error !== '' ? (
-					<Typography variant="h8" component="h8" style={{ color: 'red' }}>
-						{error}
-					</Typography>
-				) : (
-					''
-				)}
-			</div>
-			<div>
-				<Button className="login-btn" variant="contained" onClick={onClick} size="large">
-					Sign in
-				</Button>
-				<NavLink to="/register" exact>
-					<Button variant="contained" size="small">
-						Create Account
-					</Button>
-				</NavLink>
-				<NavLink to="/forgot" exact>
-					<Typography variant="h6" component="h6">
-						Forgot Password? Don't worry we back you up.
-					</Typography>
-				</NavLink>
-			</div>
-		</div>
-	);
+  const classes = useStyles();
+  const classesGlobal = globalStyles();
+
+  return (
+    <Container maxWidth={"sm"} className={classes.containerMain}>
+      <Container disableGutters className={classes.containerCentered}>
+        <Typography variant="h4">
+          Welcome back
+        </Typography>
+        <FormControl fullWidth margin={"normal"}>
+          <EmailInput
+            className={"email-textfield"}
+            helperText="Invalid Email"
+            onChange={setEmail}
+            value={email}/>
+        </FormControl>
+        <FormControl fullWidth margin={"normal"}>
+          <TextField
+            id={"pass-textfield"}
+            label="Password"
+            type="password"
+            value={password}
+            onChange={setPassword}/>
+        </FormControl>
+        <Container disableGutters>
+          {error !== '' ? (
+            <Typography color={"error"}>
+              {error}
+            </Typography>
+          ) : (
+            ''
+          )}
+        </Container>
+        <Button
+          id={"login-btn"}
+          variant="contained"
+          color={"primary"}
+          onClick={onClick}
+          size="large"
+          className={classes.button}
+          fullWidth
+        >
+          Sign in
+        </Button>
+      </Container>
+      <Container
+        disableGutters
+        className={classes.containerButtons}>
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          component={Link}
+          to="/register"
+          className={classes.button}
+          size="large"
+        >
+          Sign up
+        </Button>
+        <Typography
+          component={Link}
+          to={"/forgot"}
+          className={classes.forgot}
+        >
+          Forgot Password?
+        </Typography>
+      </Container>
+    </Container>
+  );
 }
