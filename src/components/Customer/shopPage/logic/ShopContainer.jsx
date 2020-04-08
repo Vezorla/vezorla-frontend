202fa507a15159fc0af26d03de137f24150726ea
Carlos J. Garcia Carmona <carlos.garciacarmona@edu.sait.ca>
2020-04-07 23:36:10 -0600

    UI for Shop and grid of products

--- src/components/Customer/shopPage/logic/ShopContainer.jsx
+++ src/components/Customer/shopPage/logic/ShopContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Shop from '../view/Shop';
 
@@ -15,68 +15,66 @@ const IMG_URL = 'http://localhost:8080/api/admin/img/get';
  * Shop Logic class component
  */
 class ShopContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			list: [],
-			stage: '',
-			imgs: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      stage: '',
+      imgs: []
+    };
+  }
 
-	/**
-	 * Handler for adding product to cart
-	 */
-	fetchData = async () => {
-		this.setState({ stage: 'loading' });
-		try {
-			const response = await fetch(URL);
+  /**
+   * Handler for adding product to cart
+   */
+  fetchData = async () => {
+    this.setState({stage: 'loading'});
+    try {
+      const response = await fetch(URL);
 
-			if (response.status === 200) {
-				const data = await response.json();
-				if (data !== null) {
-					let tempImgs = [];
-					for (let temp of data) {
-						tempImgs.push(temp.imageMain);
-					}
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data !== null) {
+          let tempImgs = [];
+          for (let temp of data) {
+            tempImgs.push(temp.imageMain);
+          }
 
-					this.setState({ list: data });
+          this.setState({list: data});
 
-					for (let index in tempImgs) {
-						const response = await fetch(`${IMG_URL}/${tempImgs[index]}`, {
-							method: 'GET',
-							credentials: 'include',
-							mode: 'cors'
-						});
-						const data = await response.json();
-						tempImgs[index] = data.picByte;
-					}
+          for (let index in tempImgs) {
+            const response = await fetch(`${IMG_URL}/${tempImgs[index]}`, {
+              method: 'GET',
+              credentials: 'include',
+              mode: 'cors'
+            });
+            const data = await response.json();
+            tempImgs[index] = data.picByte;
+          }
 
-					this.setState({ imgs: [ ...tempImgs ], stage: 'done' });
-				}
-			} else if (response.status >= 400) {
-				this.setState({ stage: 'error' });
-			}
-		} catch (err) {
-			this.setState({ stage: 'error' });
-		}
-	};
+          this.setState({imgs: [...tempImgs], stage: 'done'});
+        }
+      } else if (response.status >= 400) {
+        this.setState({stage: 'error'});
+      }
+    } catch (err) {
+      this.setState({stage: 'error'});
+    }
+  };
 
-	// ------fetch on load-------
-	componentDidMount() {
-		this.fetchData();
-	}
+  // ------fetch on load-------
+  componentDidMount() {
+    this.fetchData();
+  }
 
-	/**
-	 * @returns Shop component that Shop Logic wrap around Shop View
-	 */
-	render() {
-		return (
-			<div>
-				<Shop list={this.state.list} stage={this.state.stage} imgs={this.state.imgs} />
-			</div>
-		);
-	}
+  /**
+   * @returns Shop component that Shop Logic wrap around Shop View
+   */
+  render() {
+    return (
+      <Shop list={this.state.list} stage={this.state.stage} imgs={this.state.imgs}/>
+    );
+  }
 }
 
 export default ShopContainer;
--- src/components/Customer/shopPage/view/CardItem.jsx
+++ src/components/Customer/shopPage/view/CardItem.jsx
@@ -1,85 +1,78 @@
 import React from 'react';
-import { Card, Button, CardMedia, Grid, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  makeStyles,
+  Typography
+} from '@material-ui/core';
+import globalStyles from "../../../../assets/styles/styles";
 
 /**
  * @file CardItem View Component
  * @author MinhL4m
- * @version 1.0 
+ * @version 1.0
  */
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    textAlign: "center"
+  }
+}));
 
 /**
  * creat CardItem view pass on props passed in
  * @param {Object} props - props of this functional component
  * @returns CardItem View
  */
-function CardItem({ product, xs = 12, sm = 6, md = 4, img = '' }) {
-	const { prodId, name, harvestTime, price } = product;
-	const url = '/customer/product/' + prodId;
+function CardItem({product, img = ''}) {
+  const styles = useStyles();
+  const stylesGlobal = globalStyles();
+  const {prodId, name, harvestTime, price} = product;
+  const url = '/customer/product/' + prodId;
 
-	return (
-		<Grid item xs={xs} sm={sm} md={md}>
-			<Link to={url}>
-				<Card
-					key={prodId}
-					justifyContent="center"
-					disableUnderline={true}
-					style={{ border: '1px solid black', marginTop: '3em', paddingBottom: '10px', color: '#0C3658' }}
-				>
-					<CardMedia
-						component="img"
-						alt={name}
-						height="140"
-						image={`data:image/jpeg;base64,${img}`}
-						title="props.name"
-						style={{
-							width: '70%',
-							margin: 'auto',
-							marginTop: '2em',
-							marginBottom: '2em',
-							height: '40%',
-							border: '3px solid black',
-							borderRadius: '10px',
-							boxShadow: '0px 3px 5px black'
-						}}
-					/>
-					<Typography variant="h5" component="h4">
-						{name}
-					</Typography>
-					<Typography variant="h5" component="h4">
-						Harvest Time:{harvestTime}
-					</Typography>
-					<Typography variant="h5" component="h4">
-						${price}
-					</Typography>
-
-					<div
-						className="card--price"
-						style={{
-							paddingBottom: '1.5em',
-							display: 'flex',
-							justifyContent: 'center'
-						}}
-					>
-						<Button
-							size="large"
-							variant="outlined"
-							style={{
-								marginLeft: 'auto',
-								marginRight: 'auto',
-								width: '100%',
-								borderRadius: '0',
-								borderLeft: 'none',
-								borderRight: 'none',
-								borderColor: '#000'
-							}}
-						>
-							View
-						</Button>
-					</div>
-				</Card>
-			</Link>
-		</Grid>
-	);
+  return (
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+      <Link to={url} className={stylesGlobal.link}>
+        <Card
+          key={prodId}
+          raised
+        >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt={name}
+              image={`data:image/jpeg;base64,${img}`}
+              title="props.name"
+            />
+            <CardContent className={styles.content}>
+              <Typography variant="h6">
+                {name}
+              </Typography>
+              <Typography>
+                Harvested on {harvestTime}
+              </Typography>
+              <Typography>
+                <b>${price}</b>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <Button
+            color={"primary"}
+            variant="contained"
+            fullWidth
+            size={"large"}
+          >
+            View
+          </Button>
+        </Card>
+      </Link>
+    </Grid>
+  );
 }
+
 export default CardItem;
