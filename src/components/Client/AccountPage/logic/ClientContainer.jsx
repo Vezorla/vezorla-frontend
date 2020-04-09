4fc6d25dff3ecb51b42a2d9bbfd11b66b8ad547d
Carlos J. Garcia Carmona <carlos.garciacarmona@edu.sait.ca>
2020-04-08 20:23:49 -0600

    UI fix for account info tab

--- src/components/Client/AccountPage/logic/ClientContainer.jsx
+++ src/components/Client/AccountPage/logic/ClientContainer.jsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import {
+  AppBar,
+  Container,
+  Tab,
+  Tabs
+} from "@material-ui/core";
+import PopUp from '../../../common/PopUp/PopUp';
 import InfoContainer from './InfoContainer';
 import OrderHistoryContainer from './OrderHistoryContainer';
-import SwipeableViews from 'react-swipeable-views';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import PopUp from '../../../common/PopUp/PopUp';
 
 /**
  * @file Client Logic Component
@@ -14,80 +17,81 @@ import PopUp from '../../../common/PopUp/PopUp';
  */
 
 function a11yProps(index) {
-	return {
-		id: `full-width-tab-${index}`,
-		'aria-controls': `full-width-tabpanel-${index}`
-	};
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`
+  };
 }
 
 export default class ClientContainer extends Component {
-	constructor() {
-		super();
-		this.state = {
-			value: 0,
-			error: false,
-			message: '',
-			success: false
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      error: false,
+      message: '',
+      success: false
+    };
+  }
 
-	setValue = (newVal) => {
-		this.setState({ value: newVal });
-	};
-	setError = () => {
-		this.setState({ error: !this.state.error });
-	};
-	setSuccess = () => {
-		this.setState({ success: !this.state.success });
-	};
+  setValue = (newVal) => {
+    this.setState({value: newVal});
+  };
+  setError = () => {
+    this.setState({error: !this.state.error});
+  };
+  setSuccess = () => {
+    this.setState({success: !this.state.success});
+  };
 
-	setMessage = (newVal) => {
-		this.setState({ message: newVal });
-	};
+  setMessage = (newVal) => {
+    this.setState({message: newVal});
+  };
 
-	handleChange = (event, newVal) => {
-		this.setValue(newVal);
-	};
+  handleChange = (event, newVal) => {
+    this.setValue(newVal);
+  };
 
-	handleChangeIndex = (newVal) => {
-		this.setValue(newVal);
-	};
-	render() {
-		return (
-			<div>
-				{this.state.error ? (
-					<PopUp message={this.state.message} onClose={this.setError} handleOk={this.setError} />
-				) : (
-					''
-				)}
-				{this.state.success ? (
-					<PopUp
-						message={this.state.message}
-						label="Success"
-						onClose={this.setSuccess}
-						handleOk={this.setSuccess}
-					/>
-				) : (
-					''
-				)}
-				<AppBar position="static" color="default">
-					<Tabs
-						value={this.state.value}
-						onChange={this.handleChange}
-						indicatorColor="primary"
-						textColor="primary"
-						variant="fullWidth"
-						aria-label="full width tabs example"
-					>
-						<Tab label="Information" {...a11yProps(0)} />
-						<Tab label="Order History" {...a11yProps(1)} />
-					</Tabs>
-				</AppBar>
-				<SwipeableViews index={this.state.value} onChangeIndex={this.handleChangeIndex}>
-					<InfoContainer setError={this.setError} setMessage={this.setMessage} setSuccess={this.setSuccess} />
-					<OrderHistoryContainer />
-				</SwipeableViews>
-			</div>
-		);
-	}
+  handleChangeIndex = (newVal) => {
+    this.setValue(newVal);
+  };
+
+  render() {
+    return (
+      <Container disableGutters>
+        {this.state.error ? (
+          <PopUp message={this.state.message} onClose={this.setError} handleOk={this.setError}/>
+        ) : (
+          ''
+        )}
+        {this.state.success ? (
+          <PopUp
+            message={this.state.message}
+            label="Success"
+            onClose={this.setSuccess}
+            handleOk={this.setSuccess}
+          />
+        ) : (
+          ''
+        )}
+        <AppBar position="static" color="default">
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="Information" {...a11yProps(0)} />
+            <Tab label="Order History" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews index={this.state.value} onChangeIndex={this.handleChangeIndex}>
+          <InfoContainer setError={this.setError} setMessage={this.setMessage} setSuccess={this.setSuccess}/>
+          <OrderHistoryContainer/>
+        </SwipeableViews>
+      </Container>
+    );
+  }
 }
--- src/components/Client/AccountPage/view/Info.jsx
+++ src/components/Client/AccountPage/view/Info.jsx
@@ -1,57 +1,70 @@
 import React from 'react';
 import InputGroupRequired from '../../../common/Inputs/NecessaryInput/InputGroupRequired';
 import LoadingHOC from '../../../common/HOC/LoadingHOC';
-import { Button, FormControlLabel, Switch } from '@material-ui/core';
+import {Button, Container, FormControlLabel, Switch} from '@material-ui/core';
+
 /**
- * @file Info Component 
+ * @file Info Component
  * @author MinhL4m
  * @version 1.0
  */
 
-const InfoComponent = ({
-	info,
-	setFirstname,
-	setLastname,
-	setPhone,
-	setAddress,
-	setCity,
-	setProvince,
-	setPostalCode,
-	setCountry,
-	setPassword,
-	setSubscription,
-	onClick
-}) => {
-	return (
-		<div>
-			<InputGroupRequired
-				info={info}
-				setAddress={setAddress}
-				setCity={setCity}
-				setCountry={setCountry}
-				setFirstname={setFirstname}
-				setLastname={setLastname}
-				setPassword={setPassword}
-				setPhone={setPhone}
-				setPostalCode={setPostalCode}
-				setProvince={setProvince}
-				disbaledEmail={true}
-			/>
-			<FormControlLabel
-				control={<Switch checked={info.subscription} onChange={setSubscription} color="primary" />}
-				label="Subscription to mailing list"
-				labelPlacement="start"
-			/>
+const containerStyle = {
+	textAlign: "center"
+};
+const buttonStyle = {
+  margin: "1rem 0"
+};
 
-			<Button variant="contained" onClick={onClick}>
-				Save
-			</Button>
-		</div>
-	);
+const InfoComponent = ({
+                         info,
+                         setFirstname,
+                         setLastname,
+                         setPhone,
+                         setAddress,
+                         setCity,
+                         setProvince,
+                         setPostalCode,
+                         setCountry,
+                         setPassword,
+                         setSubscription,
+                         onClick
+                       }) => {
+  return (
+    <Container maxWidth={"sm"} style={containerStyle}>
+      <InputGroupRequired
+        info={info}
+        setAddress={setAddress}
+        setCity={setCity}
+        setCountry={setCountry}
+        setFirstname={setFirstname}
+        setLastname={setLastname}
+        setPassword={setPassword}
+        setPhone={setPhone}
+        setPostalCode={setPostalCode}
+        setProvince={setProvince}
+        disbaledEmail={true}
+      />
+      <FormControlLabel
+        control={<Switch checked={info.subscription} onChange={setSubscription} color="primary"/>}
+        label="Subscription to mailing list"
+        labelPlacement="start"
+      />
+      <Button
+        variant="contained"
+        color={"primary"}
+        fullWidth
+        onClick={onClick}
+        style={buttonStyle}
+      >
+        Save
+      </Button>
+    </Container>
+  );
 };
 
 const Info = (props) => {
-	return LoadingHOC(InfoComponent)({ ...props, message: 'something went wrong with loading information' });
+  return LoadingHOC(InfoComponent)({...props, message: 'something went wrong with loading information'});
 };
 
 export default Info;
